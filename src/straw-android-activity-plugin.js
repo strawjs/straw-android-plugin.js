fd28bfe083dac0d19b3badde14b287fcbe090d52
Yosiya Hinosawa <stibium121@gmail.com>
2013-10-19 13:42:53 +0900

    Add placeholder for activity and optionsMenu plugins

---
+++ src/straw-android-activity-plugin.js
@@ -0,0 +1,12 @@
+// straw-android-activity-plugin
+
+window.straw.activity= (function (straw, $) {
+    'use strict';
+
+    var PLUGIN_NAME = 'activity';
+
+    var exports = {
+    };
+
+    return exports;
+}(window.straw, window.$));
---
+++ src/straw-android-options-menu-plugin.js
@@ -0,0 +1,12 @@
+// straw-android-options-menu-plugin
+
+window.straw.optionsMenu = (function (straw, $) {
+    'use strict';
+
+    var PLUGIN_NAME = 'optionsMenu';
+
+    var exports = {
+    };
+
+    return exports;
+}(window.straw, window.$));
