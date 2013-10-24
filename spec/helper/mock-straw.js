2479ac9ffb4a93e5e06f246d31ce7b76f2950c3b
Yosiya Hinosawa <stibium121@gmail.com>
2013-10-25 00:54:22 +0900

    Add success callback test

--- spec/helper/mock-straw.js
+++ spec/helper/mock-straw.js
@@ -1,4 +1,4 @@
 
-window.JS_TO_NATIVE_INTERFACE = {
+window.straw.JS_TO_NATIVE_INTERFACE = window.JS_TO_NATIVE_INTERFACE = {
     exec: function () {}
 };
--- spec/straw-android-http-plugin-spec.js
+++ spec/straw-android-http-plugin-spec.js
@@ -33,14 +33,35 @@ describe('http', function () {
 
             var spy = sinon.spy(straw, 'exec');
 
-            straw.http.get('http://example.com/');
+            straw.http.get('http://example.com/', 123, 'shift_jis');
 
             expect(spy.getCall(0).args[0]).toBe('http');
             expect(spy.getCall(0).args[1]).toBe('get');
             expect(typeof spy.getCall(0).args[2]).toBe('object');
             expect(spy.getCall(0).args[2].url).toBe('http://example.com/');
+            //expect(spy.getCall(0).args[2].timeout).toBe(123);
+            //expect(spy.getCall(0).args[2].charset).toBe('shitf_jis');
 
             straw.exec.restore();
+
+        });
+
+        describe('success handler', function () {
+
+            it('will be called', function () {
+
+                var spy = sinon.spy(straw.JS_TO_NATIVE_INTERFACE, 'exec');
+                var spy2 = sinon.spy();
+                spy2 = spy2;
+
+                straw.http.get('http://example.com/', 123, 'shift_jis').done(spy2);
+
+                var callbackId = spy.getCall(0).args[3];
+
+                straw.NATIVE_TO_JS_INTERFACE.exec(callbackId, true, {}, false);
+
+                expect(spy2.calledOnce).toBe(true);
+            });
 
         });
 
