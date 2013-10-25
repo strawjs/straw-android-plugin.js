975259d3a23b281bfb47490906d8605e55d9cd36
Yosiya Hinosawa <stibium121@gmail.com>
2013-10-25 22:36:02 +0900

    Add straw.log tests

---
+++ spec/straw-android-log-plugin-spec.js
@@ -0,0 +1,116 @@
+
+var describe = window.describe;
+var it = window.it;
+var expect = window.expect;
+
+var sinon = window.sinon;
+
+var straw = window.straw;
+
+
+describe('log', function () {
+    'use strict';
+
+    it('exists', function () {
+
+        expect(straw.log).not.toEqual(null);
+        expect(typeof straw.log).toEqual('object');
+
+    });
+
+    describe('verbose', function () {
+
+        it('calls straw.exec with appropriate arguments', function () {
+            var spy = sinon.spy(straw, 'exec');
+
+            straw.log.verbose('abc');
+
+            var args = spy.getCall(0).args;
+
+            expect(args[0]).toBe('log');
+            expect(args[1]).toBe('verbose');
+            expect(args[2].message).toBe('abc');
+            expect(args[2].label).toBe('straw');
+
+            spy.restore();
+        });
+
+    });
+
+    describe('debug', function () {
+
+        it('calls straw.exec with appropriate arguments', function () {
+            var spy = sinon.spy(straw, 'exec');
+
+            straw.log.debug('abc');
+
+            var args = spy.getCall(0).args;
+
+            expect(args[0]).toBe('log');
+            expect(args[1]).toBe('debug');
+            expect(args[2].message).toBe('abc');
+            expect(args[2].label).toBe('straw');
+
+            spy.restore();
+        });
+
+    });
+
+    describe('info', function () {
+
+        it('calls straw.exec with appropriate arguments', function () {
+            var spy = sinon.spy(straw, 'exec');
+
+            straw.log.info('abc');
+
+            var args = spy.getCall(0).args;
+
+            expect(args[0]).toBe('log');
+            expect(args[1]).toBe('info');
+            expect(args[2].message).toBe('abc');
+            expect(args[2].label).toBe('straw');
+
+            spy.restore();
+        });
+
+    });
+
+    describe('warn', function () {
+
+        it('calls straw.exec with appropriate arguments', function () {
+            var spy = sinon.spy(straw, 'exec');
+
+            straw.log.warn('abc');
+
+            var args = spy.getCall(0).args;
+
+            expect(args[0]).toBe('log');
+            expect(args[1]).toBe('warn');
+            expect(args[2].message).toBe('abc');
+            expect(args[2].label).toBe('straw');
+
+            spy.restore();
+        });
+
+    });
+
+    describe('error', function () {
+
+        it('calls straw.exec with appropriate arguments', function () {
+            var spy = sinon.spy(straw, 'exec');
+
+            straw.log.error('abc');
+
+            var args = spy.getCall(0).args;
+
+            expect(args[0]).toBe('log');
+            expect(args[1]).toBe('error');
+            expect(args[2].message).toBe('abc');
+            expect(args[2].label).toBe('straw');
+
+            spy.restore();
+        });
+
+    });
+
+});
--- src/straw-android-log-plugin.js
+++ src/straw-android-log-plugin.js
@@ -11,40 +11,48 @@ window.straw.log = (function (straw) {
     var ACTION_WARN = 'warn';
     var ACTION_ERROR = 'error';
 
+    var DEFAULT_LABEL = 'straw';
+
     var exports = {
+
+        label: DEFAULT_LABEL,
+
+        setLabel: function (label) {
+            this.label = label;
+        },
 
         verbose: function (message, label) {
             straw.exec(PLUGIN_NAME, ACTION_VERBOSE, {
                 message: message,
-                label: label
+                label: label || this.label
             });
         },
 
         debug: function (message, label) {
             straw.exec(PLUGIN_NAME, ACTION_DEBUG, {
                 message: message,
-                label: label
+                label: label || this.label
             });
         },
 
         info: function (message, label) {
             straw.exec(PLUGIN_NAME, ACTION_INFO, {
                 message: message,
-                label: label
+                label: label || this.label
             });
         },
 
         warn: function (message, label) {
             straw.exec(PLUGIN_NAME, ACTION_WARN, {
                 message: message,
-                label: label
+                label: label || this.label
             });
         },
 
         error: function (message, label) {
             straw.exec(PLUGIN_NAME, ACTION_ERROR, {
                 message: message,
-                label: label
+                label: label || this.label
             });
         }
 
