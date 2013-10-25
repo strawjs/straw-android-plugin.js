d0109118b7751937948e5253af92451d80148b88
Yosiya Hinosawa <stibium121@gmail.com>
2013-10-25 14:26:24 +0900

    Add error handler test and fix some assertions

--- spec/straw-android-http-plugin-spec.js
+++ spec/straw-android-http-plugin-spec.js
@@ -39,8 +39,8 @@ describe('http', function () {
             expect(spy.getCall(0).args[1]).toBe('get');
             expect(typeof spy.getCall(0).args[2]).toBe('object');
             expect(spy.getCall(0).args[2].url).toBe('http://example.com/');
-            //expect(spy.getCall(0).args[2].timeout).toBe(123);
-            //expect(spy.getCall(0).args[2].charset).toBe('shitf_jis');
+            expect(spy.getCall(0).args[2].timeout).toBe(123);
+            expect(spy.getCall(0).args[2].charset).toBe('shift_jis');
 
             straw.exec.restore();
 
@@ -48,19 +48,44 @@ describe('http', function () {
 
         describe('success handler', function () {
 
-            it('will be called', function () {
+            it('will be called, when plugin execution succeeded', function () {
 
                 var spy = sinon.spy(straw.JS_TO_NATIVE_INTERFACE, 'exec');
                 var spy2 = sinon.spy();
-                spy2 = spy2;
+                var spy3 = sinon.spy();
 
-                straw.http.get('http://example.com/', 123, 'shift_jis').done(spy2);
+                straw.http.get('http://example.com/', 123, 'shift_jis').done(spy2).fail(spy3);
 
                 var callbackId = spy.getCall(0).args[3];
 
                 straw.NATIVE_TO_JS_INTERFACE.exec(callbackId, true, {}, false);
 
                 expect(spy2.calledOnce).toBe(true);
+                expect(spy3.called).toBe(false);
+
+                spy.restore();
+            });
+
+        });
+
+        describe('failure handler', function () {
+
+            it('will be called, when plugin execution failed', function () {
+
+                var spy = sinon.spy(straw.JS_TO_NATIVE_INTERFACE, 'exec');
+                var spy2 = sinon.spy();
+                var spy3 = sinon.spy();
+
+                straw.http.get('http://example.com/', 123, 'shift_jis').done(spy2).fail(spy3);
+
+                var callbackId = spy.getCall(0).args[3];
+
+                straw.NATIVE_TO_JS_INTERFACE.exec(callbackId, false, {}, false);
+
+                expect(spy2.called).toBe(false);
+                expect(spy3.calledOnce).toBe(true);
+
+                spy.restore();
             });
 
         });
