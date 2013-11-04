d730714c53525b4ba76bee049f5558cf959ac5c0
Yoshiya Hinosawa <stibium121@gmail.com>
2013-11-04 18:35:39 +0900

    Fix return values of sharedPreferences plugin actions

--- src/straw-android-shared-preferences-plugin.js
+++ src/straw-android-shared-preferences-plugin.js
@@ -12,34 +12,63 @@ window.straw.sharedPreferences = (function (straw, $) {
     var ACTION_DUMP = 'dump';
 
     var exports = {
+
+        /**
+         * @param {string} key the key to get
+         * @param {object} value default value in the case the key doesn't exist
+         * @return {promise}
+         *     @done
+         *         @param {object} value the got value
+         *     @fail
+         *         @param {object} error
+         *         @subparam {string} error.code error code
+         *         @subparam {string} error.message
+         */
         get: function (key, value) {
             var d = $.Deferred();
 
             straw.exec(PLUGIN_NAME, ACTION_GET, {
                 key: key,
                 value: JSON.stringify(value)
+
             }, function (x) {
                 x = JSON.parse(x.value);
                 d.resolve(x);
+
             }, function (x) {
                 d.reject(x);
+
             });
 
-            return d;
+            return d.promise();
         },
 
+        /**
+         * set value for key to SharedPreferences
+         * @param {string} key the key to set
+         * @param {object} value the value to set
+         * @return {promise}
+         *     @done
+         *         @param {boolean} value if successfully set or not
+         *     @fail
+         *         @param {object} error
+         *         @subparam {string} error.code error code
+         *         @subparam {string} error.massage error message
+         */
         set: function (key, value) {
             var d = $.Deferred();
 
             straw.exec(PLUGIN_NAME, ACTION_SET, {
                 key: key,
                 value: JSON.stringify(value)
+
             }, function (x) {
                 d.resolve(x.value);
             }, function (x) {
                 d.reject(x);
             });
 
+            return d.promise();
         },
 
         has: function (key) {
@@ -54,6 +83,7 @@ window.straw.sharedPreferences = (function (straw, $) {
                 d.reject(x);
             });
 
+            return d.promise();
         },
 
         clear: function () {
@@ -65,6 +95,7 @@ window.straw.sharedPreferences = (function (straw, $) {
                 d.reject(x);
             });
 
+            return d.promise();
         },
 
         remove: function (key) {
@@ -78,6 +109,7 @@ window.straw.sharedPreferences = (function (straw, $) {
                 d.reject(x);
             });
 
+            return d.promise();
         },
 
         dump: function () {
@@ -89,9 +121,11 @@ window.straw.sharedPreferences = (function (straw, $) {
                 d.reject(x);
             });
 
+            return d.promise();
         }
 
     };
 
     return exports;
+
 }(window.straw, window.$));
