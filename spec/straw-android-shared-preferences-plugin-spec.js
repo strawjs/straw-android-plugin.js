826517c80890d882da51e630e1baeb96c6999366
Yoshiya Hinosawa <stibium121@gmail.com>
2013-11-04 19:00:04 +0900

    Add sharedPreferences tests

--- spec/straw-android-shared-preferences-plugin-spec.js
+++ spec/straw-android-shared-preferences-plugin-spec.js
@@ -50,4 +50,150 @@ describe('http', function () {
         });
 
     });
+
+
+    describe('set', function () {
+
+        it('is defined and is a function', function () {
+
+            expect(typeof straw.sharedPreferences.set).toBe('function');
+
+        });
+
+        it('invokes straw.execute with appropriate arguments', function () {
+
+            var spy = sinon.spy(straw, 'exec');
+
+            straw.sharedPreferences.set('a_key', {a: 123});
+
+            // sharedPreferences plugin's set action
+            expect(spy.getCall(0).args[0]).toBe('sharedPreferences');
+            expect(spy.getCall(0).args[1]).toBe('set');
+
+            // arguments assertions
+            expect(typeof spy.getCall(0).args[2]).toBe('object');
+
+            // key
+            expect(spy.getCall(0).args[2].key).toBe('a_key');
+
+            // value is JSON.stringified
+            expect(spy.getCall(0).args[2].value).toBe('{"a":123}');
+
+            straw.exec.restore();
+
+        });
+
+    });
+
+
+    describe('has', function () {
+
+        it('is defined and is a function', function () {
+
+            expect(typeof straw.sharedPreferences.has).toBe('function');
+
+        });
+
+        it('invokes straw.execute with appropriate arguments', function () {
+
+            var spy = sinon.spy(straw, 'exec');
+
+            straw.sharedPreferences.has('a_key');
+
+            // sharedPreferences plugin's has action
+            expect(spy.getCall(0).args[0]).toBe('sharedPreferences');
+            expect(spy.getCall(0).args[1]).toBe('has');
+
+            // arguments assertions
+            expect(typeof spy.getCall(0).args[2]).toBe('object');
+
+            // key
+            expect(spy.getCall(0).args[2].key).toBe('a_key');
+
+            straw.exec.restore();
+
+        });
+
+    });
+
+
+    describe('clear', function () {
+
+        it('is defined and is a function', function () {
+
+            expect(typeof straw.sharedPreferences.clear).toBe('function');
+
+        });
+
+        it('invokes straw.execute with appropriate arguments', function () {
+
+            var spy = sinon.spy(straw, 'exec');
+
+            straw.sharedPreferences.clear();
+
+            // sharedPreferences plugin's remove action
+            expect(spy.getCall(0).args[0]).toBe('sharedPreferences');
+            expect(spy.getCall(0).args[1]).toBe('clear');
+
+            straw.exec.restore();
+
+        });
+
+    });
+
+
+    describe('remove', function () {
+
+        it('is defined and is a function', function () {
+
+            expect(typeof straw.sharedPreferences.remove).toBe('function');
+
+        });
+
+        it('invokes straw.execute with appropriate arguments', function () {
+
+            var spy = sinon.spy(straw, 'exec');
+
+            straw.sharedPreferences.remove('a_key', {a: 123});
+
+            // sharedPreferences plugin's remove action
+            expect(spy.getCall(0).args[0]).toBe('sharedPreferences');
+            expect(spy.getCall(0).args[1]).toBe('remove');
+
+            // arguments assertions
+            expect(typeof spy.getCall(0).args[2]).toBe('object');
+
+            // key
+            expect(spy.getCall(0).args[2].key).toBe('a_key');
+
+            straw.exec.restore();
+
+        });
+
+    });
+
+
+    describe('dump', function () {
+
+        it('is defined and is a function', function () {
+
+            expect(typeof straw.sharedPreferences.dump).toBe('function');
+
+        });
+
+        it('invokes straw.execute with appropriate arguments', function () {
+
+            var spy = sinon.spy(straw, 'exec');
+
+            straw.sharedPreferences.dump();
+
+            // sharedPreferences plugin's dump action
+            expect(spy.getCall(0).args[0]).toBe('sharedPreferences');
+            expect(spy.getCall(0).args[1]).toBe('dump');
+
+            straw.exec.restore();
+
+        });
+
+    });
 });
